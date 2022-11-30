f392f8c991165799c68887f0acbbed819a5a0cc9
di3goCS <diego.cs282@gmail.com>
2022-11-30 00:45:29 -0300

    change broker info

--- tmo.js
+++ tmo.js
@@ -43,7 +43,7 @@
 			};
 			mosq.ondisconnect = function(rc){
 				var p = document.createElement("p");
-				var url = "ws://iot.eclipse.org/ws";
+				var url = "tcp://10.0.0.101";
 				
 				p.innerHTML = "A conexão com o broker foi perdida";
 				$("#debug").append(p);				
@@ -63,7 +63,7 @@
 			};
 		}
 		Page.prototype.connect = function(){
-			var url = "ws://iot.eclipse.org/ws";
+			var url = "tcp://10.0.0.101";
 			mosq.connect(url);
 		};
 		Page.prototype.disconnect = function(){
