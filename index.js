4434c601316f403aeb4d9bea6a6b606dc2fb00e5
di3goCS <diego.cs282@gmail.com>
2022-12-07 09:26:09 -0300

    add minified files

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ class PahoHandler {
       this.host = host;
       this.port = port;
       this.clientId = clientId;
-      this.client = new Paho.Client(this.host, this.port, this.clientId);
+      this.client = new Paho.MQTT.Client(this.host, this.port, this.clientId);
       this.client.connect({
+        userName: "aluno",
+        password: "@luno*123",
         onSuccess: this.onSuccess,
         timeout: 3,
       });
@@ -29,7 +31,8 @@ class PahoHandler {
   
   const array = [1, 2, 3, 4];
   
-  const pahoHandler = new PahoHandler('test.mosquitto.org', 8080, "clientjs");
+  // const pahoHandler = new PahoHandler('test.mosquitto.org', 8080, "clientjs");
+  const pahoHandler = new PahoHandler('10.0.0.101', 9001, "clientjs");
   
   
   window.addEventListener('DOMContentLoaded', () => {
@@ -40,6 +43,7 @@ class PahoHandler {
     const sendButton = document.querySelector('#send-button');
     const topicInput = document.querySelector('#topic');
     const messages = document.querySelector('#messages');
+    const chartdiv = document.querySelector("#chart");
   
     subscribeButton.addEventListener('click', () => {
       pahoHandler.subscribe(subscribeInput.value);
@@ -49,9 +53,24 @@ class PahoHandler {
       pahoHandler.send(topicInput.value, messageInput.value);
     });
     const onMessage = (message) => {
+      console.log(message)
       const messageElement = document.createElement('li');
       messageElement.innerHTML = message.payloadString;
       messages.appendChild(messageElement);
+      chart = Plot.plot({
+        marks: [
+          Plot.line(sales, {x: "date", y: "sales", stroke: "fruit"}),
+          Plot.dot(sales, {x: "date", y: "sales", r: 10, fill: "white"}),
+          Plot.text(sales, {x: "date", y: "sales", text: "sales"}),
+          Plot.text(sales, Plot.selectLast({
+            x: "date", y: "sales", text: "fruit", z: "fruit",
+            textAnchor: "start", dx: 10, fontWeight: "bold"
+          }))
+        ],
+        y: {axis: null, inset: 15},
+        marginRight: 55
+      });
+      chartdiv.appendChild(chart);
     };
   
     pahoHandler.registerOnMessageArrived(onMessage);
