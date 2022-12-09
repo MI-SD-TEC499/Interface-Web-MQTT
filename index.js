1660af08e0c8213d5cff34eaa2625f3db1b3ea14
Daniel <38465439+dalvesc@users.noreply.github.com>
2022-12-09 19:06:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 class PahoHandler {
+    //definindo configurações para a conexão ao MQTT
     constructor(host, port, clientId) {
       this.host = host;
       this.port = port;
@@ -11,29 +12,30 @@ class PahoHandler {
         timeout: 3,
       });
     }
-  
+    //quando consegue se conectar aparece a mensagem no terminal do console
     onSuccess() {
       console.log('connected');
     }
-  
+    //mensagem recebida
     registerOnMessageArrived(fn) {
       this.client.onMessageArrived = fn;
     }
-  
+    //se inscrevendo no tópico
     subscribe(topic) {
       this.client.subscribe(topic);
     }
-  
+    //função para enviar a mensagem para o tópico
     send(topic, message) {
       this.client.send(topic, message);
     }
   }
-  
+  //variáveis para armazenar os valores 
   var umidity = [];
   var temperature = [];
   var potenciometer = [];
   
-  // const pahoHandler = new PahoHandler('test.mosquitto.org', 8080, "clientjs");
+   //conexão com o broker
+  // const pahoHandler = new PahoHandler('test.mosquitto.org', 8080, "clientjs"); //broker remoto para teste
   const pahoHandler = new PahoHandler('10.0.0.101', 9001, "clientjs");
   
   
@@ -44,27 +46,29 @@ class PahoHandler {
     const messageInput = document.querySelector('#message');
     const sendButton = document.querySelector('#send-button');
     const topicInput = document.querySelector('#topic');
-  
+    //quando apertar o botão irá chamar a função de subinscrever
     subscribeButton.addEventListener('click', () => {
       pahoHandler.subscribe(subscribeInput.value);
     });
-  
+    //quando apertar o botão irá chamar a função de enviar mensagem
     sendButton.addEventListener('click', () => {
       pahoHandler.send(topicInput.value, messageInput.value);
     });
+    //mensagem recebida através do tópico de subinscrita
     const onMessage = (message) => {
       var payload = message.payloadString;
-      splited = payload.split(";");
+      splited = payload.split(";"); //separando os valores recebidos no array
       if (umidity.length == 10) {
         umidity.shift();
         temperature.shift();
         potenciometer.shift();
       }
+      //adicionando o valor recebido no gráfico
       umidity.push(Number(splited[0]));
       temperature.push(Number(splited[1]));
       potenciometer.push(Number(splited[2]));
 
-      linechart.update();
+      linechart.update(); //atualizando o gráfico
     };
   
     pahoHandler.registerOnMessageArrived(onMessage);
